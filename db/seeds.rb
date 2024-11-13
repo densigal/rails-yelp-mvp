4d2eb318b007985da1256fb518a5378ae84a70ad
Denis Sigal <182473309+densigal@users.noreply.github.com>
2024-11-13 14:08:48 +0900

    restaurant seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning the DB..."
+Restaurant.destroy_all
+
+CHEFS = %w[Adam Arisa Bart Brian Chafique Christian Claudia Denis Dylan Esteban Henry Jarod Loris Mark Mason PhyuPhyu Roman Steven Stuart Toby Tristan Will]
+CATEGORIES = %W[chinese italian japanese french belgian]
+
+def get_category(name)
+  last_word = name.split.last.downcase
+  CATEGORIES.include?(last_word) ? last_word : CATEGORIES.sample
+end
+
+puts "Creating Restaurants..."
+  CHEFS.shuffle.each do |name|
+    restaurant_name = Faker::Restaurant.unique.name
+    Restaurant.create!(
+    name: "#{CHEFS.sample}'s #{restaurant_name}",
+    phone_number: "#{rand(10 ** 10)}",
+    address: "#{rand(1..3)}-#{rand(1..11)} Meguro, Meguro ku, Tokyo, Japan",
+    category: get_category(restaurant_name)
+  )
+  end
+  # puts "Creating #{CHEFS.count} Restaurants..."
+  # CHEFS.shuffle.each do |name|
+  #   restaurant_name = Faker::Restaurant.unique.name
+  #   Restaurant.create!(
+  #     name: "#{name}'s #{restaurant_name}",
+  #     address: "#{rand(1..3)}-#{rand(1..11)} Meguro, Meguro ku, Tokyo, Japan",
+  #     category: get_category(restaurant_name)
+  #   )
+  # end
+puts "... created #{Restaurant.count} restaurants"
